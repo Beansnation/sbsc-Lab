12221a1033b82d1a3732a2b098d047cdc6bde516
David Adegborioye <71905015+Beansnation@users.noreply.github.com>
2021-04-25 14:48:56 +0100

    Update lab3.dart
    updating lab 3 assignment. adding comments and user prompts

--- lab3.dart
+++ lab3.dart
@@ -1,3 +1,9 @@
+/*
+Given an array arr[] of size N which contains the marks of a student in N subjects, the task is to calculate the CGPA and the CGPA percentage of the student. 
+Note: Consider all marks to be out of 100, for each subject.
+This is just for assignment purposes. It’s not the accurate reflection of the conventional CGPA in  Nigeria
+*/
+
 import "dart:io";
 
 void main (){
@@ -6,7 +12,8 @@ void main (){
    double aggregatecgpa;
    double totalGradepoints= 0;
    double percent;
-
+  
+//Create a List of grades collected from the user
   var marks = List (5);
 
 marks[0]= prompt("input first subject grade");
@@ -26,7 +33,7 @@ marks[4]= prompt("input fifth subject grade");;
   print("The aggregate cgpa is $aggregatecgpa and the percentage cgpa is $percent percent");
 
 }
-
+//create a method that prints instruction and collects data of grades from the user
  double prompt(String text, [double answer]){
  print(text);
   answer = double.parse(stdin.readLineSync());
